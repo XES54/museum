c55e5f843b167d6be90a6a79c917f81509e95973
XES54 <112929514+XES54@users.noreply.github.com>
2023-04-25 12:22:55 +0200

    etape 5

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,21 @@ class _ArtworkState extends State<Artwork> {
     content: Text('Oeuvre ajoutée à vos favoris'),
     duration: Duration(seconds: 1),
   );
+  var desc =
+      """La Joconde, ou Portrait de Mona Lisa, est un tableau de l'artiste Léonard de Vinci, réalisé
+entre 1503 et 1506 ou entre 1513 et 15161,2, et peut-être jusqu'à 1517 (l'artiste étant mort
+le 2 mai 1519), qui représente un portrait mi-corps, probablement celui de la Florentine Lisa
+Gherardini, épouse de Francesco del Giocondo. Acquise par François Ier, cette peinture à
+l'huile sur panneau de bois de peuplier de 77 × 53 cm est exposée au musée du Louvre à
+Paris. La Joconde est l'un des rares tableaux attribués de façon certaine à Léonard de Vinci.
+La Joconde est devenue un tableau éminemment célèbre car, depuis sa réalisation, nombre
+d'artistes l'ont pris comme référence. À l'époque romantique, les artistes ont été fascinés par
+ce tableau et ont contribué à développer le mythe qui l'entoure, en faisant de ce tableau
+l’une des œuvres d'art les plus célèbres du monde, si ce n'est la plus célèbre : elle est en tout
+cas considérée comme l'une des représentations d'un visage féminin les plus célèbres au
+monde. Au xxie siècle, elle est devenue l'objet d'art le plus visité au monde, devant le
+diamant Hope, avec 20 000 visiteurs qui viennent l'admirer et la photographier
+quotidiennement""";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,6 +99,10 @@ class _ArtworkState extends State<Artwork> {
               },
             )
           ]),
+          Visibility(
+            visible: _showDescription,
+            child: Text(desc),
+          ),
         ]));
   }
 }

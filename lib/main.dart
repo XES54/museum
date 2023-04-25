6ff3b9b4be8775c03a2900415943592c7545f336
XES54 <112929514+XES54@users.noreply.github.com>
2023-04-25 14:23:31 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -54,16 +54,32 @@ quotidiennement""";
           backgroundColor: const Color.fromARGB(255, 54, 16, 0),
         ),
         body: Column(children: [
-          Stack(alignment: Alignment.center, children: [
-            Image.asset('assets/images/Mona_Lisa.jpg'),
-            Icon(
-              Icons.favorite,
-              size: 100,
-              color: _isFavorite
-                  ? const Color.fromARGB(255, 238, 2, 2)
-                  : const Color.fromARGB(190, 255, 255, 255),
+          Center(
+            child: SizedBox(
+              height: 630,
+              child: Stack(alignment: Alignment.center, children: [
+                Image.asset('assets/images/Mona_Lisa.jpg'),
+                !_showDescription
+                    ? Icon(
+                        Icons.favorite,
+                        size: 100,
+                        color: _isFavorite
+                            ? const Color.fromARGB(255, 238, 2, 2)
+                            : const Color.fromARGB(190, 255, 255, 255),
+                      )
+                    : const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                        child: SingleChildScrollView(
+                          child: Text(
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white),
+                              textAlign: TextAlign.justify,
+                              "La Joconde, ou Portrait de Mona Lisa, est un tableau de l'artiste Léonard de Vinci, réalisé entre 1503 et 1506 ou entre 1513 et 1516 1,2, et peut-être jusqu'à 1517 (l'artiste étant mort le 2 mai 1519), qui représente un portrait mi-corps, probablement celui de la Florentine Lisa Gherardini, épouse de Francesco del Giocondo. Acquise par François Ier, cette peinture à l'huile sur panneau de bois de peuplier de 77 × 53 cm est exposée au musée du Louvre à Paris. La Joconde est l'un des rares tableaux attribués de façon certaine à Léonard de Vinci. La Joconde est devenue un tableau éminemment célèbre car, depuis sa réalisation, nombre d'artistes l'ont pris comme référence. À l'époque romantique, les artistes ont été fascinés par ce tableau et ont contribué à développer le mythe qui l'entoure, en faisant de ce tableau l’une des œuvres d'art les plus célèbres du monde, si ce n'est la plus célèbre : elle est en tout cas considérée comme l'une des représentations d'un visage féminin les plus célèbres au monde. Au xxie siècle, elle est devenue l'objet d'art le plus visité au monde, devant le diamant Hope, avec 20 000 visiteurs qui viennent l'admirer et la photographier quotidiennement."),
+                        ))
+              ]),
             ),
-          ]),
+          ),
           const Text('Mona Lisa',
               style: TextStyle(
                   fontSize: 30,
@@ -99,10 +115,10 @@ quotidiennement""";
               },
             )
           ]),
-          Visibility(
-            visible: _showDescription,
-            child: Text(desc),
-          ),
+          //         Visibility(
+          //          visible: _showDescription,
+          //          child: Text(desc),
+          //       ),
         ]));
   }
 }
